1f957061f7230bff336ab9972a38dbc017ef0539
Shahzad Khan <shahzadkhan3iii7@gmail.com>
2017-01-17 14:45:47 -0600

    done

--- app.js
+++ app.js
@@ -6,10 +6,19 @@
             controller: function() {
                 const vm = this;
                 vm.posts = [];
+                vm.comments = [];
                 vm.addPost = function addPost() {
-                    vm.posts.push(vm.post)
-                    delete vm.post //this is so it deletes the object so you start over with the next post. Instead of having the post repeated.
+                    vm.post.created_at = Date.now();
+                    vm.posts.push(vm.post);
+                    delete vm.post; //this is so it deletes the object so you start over with the next post. Instead of having the post repeated.
                     console.log(vm.posts);
+                }
+                vm.addComment = function addComment() {
+                    vm.comment.created_at = Date.now();
+                    vm.comments.push(vm.comment);
+                    delete vm.comment;
+                    console.log(vm.comments);
+
                 }
 
             },
