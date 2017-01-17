908916b2e435aedf5264507870349d8d966b376c
Shahzad Khan <shahzadkhan3iii7@gmail.com>
2017-01-16 18:47:39 -0600

    posts work now

--- app.js
+++ app.js
@@ -3,8 +3,18 @@
 
     angular.module("app", [])
         .component('postForm', {
-            controller: function() {},
+            controller: function() {
+                const vm = this;
+                vm.posts = [];
+                vm.addPost = function addPost() {
+                    vm.posts.push(vm.post)
+                    delete vm.post //this is so it deletes the object so you start over with the next post. Instead of having the post repeated.
+                    console.log(vm.posts);
+                }
+
+            },
             templateUrl: "postForm.html",
         });
+
 
 }())
